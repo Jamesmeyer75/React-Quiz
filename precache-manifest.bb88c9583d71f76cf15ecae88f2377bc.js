6779d4f33915e46613fb749896b7f573b63331a4
James Meyer <Jamesmeyer75@gmail>
2020-07-20 23:00:58 -0500

    Updates

---
+++ precache-manifest.bb88c9583d71f76cf15ecae88f2377bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db12d4421fdf8f616a648139f8b7a205",
+    "url": "/React-Quiz/index.html"
+  },
+  {
+    "revision": "2991f8fd673837bbf1bc",
+    "url": "/React-Quiz/static/js/2.75ceb8a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Quiz/static/js/2.75ceb8a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac21e547bec18afa09c2",
+    "url": "/React-Quiz/static/js/main.98571bee.chunk.js"
+  },
+  {
+    "revision": "beaf62e8a6e21a3aa736",
+    "url": "/React-Quiz/static/js/runtime-main.c47d5583.js"
+  },
+  {
+    "revision": "7bcb2065d56174fe2dcd263da4dd1039",
+    "url": "/React-Quiz/static/media/backdrop.7bcb2065.JPEG"
+  }
+]);
